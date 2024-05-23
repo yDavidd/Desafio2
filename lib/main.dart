05a9ae5026038b76347951af3c37c42ee16c23a6
yDavidd <132359951+yDavidd@users.noreply.github.com>
2024-05-23 12:02:42 -0300

    Desafio 2 DAVID

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:desafio2/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
